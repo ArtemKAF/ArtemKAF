c8fe18e77cd047ccc7bb11cdbbffd5f731af7388
ArtemKAF <grot_artem@inbox.ru>
2022-06-21 20:03:23 +0300

    Edit program

--- main.py
+++ main.py
@@ -2,17 +2,23 @@
 #Без обработки ошибок
 #Автор ArtemKAF
 
-def num_sys_conv(number, num_sys1, num_sys2):
+def bin2dec(number):
     c_number = number
-    print(c_number)
+    print(c_number, type(c_number))
 
+def dec2bin(number):
+    c_number = number
+    print(c_number, type(c_number))
 
 if __name__ == '__main__':
-    print("Введите число для конвертации:")
-    number = input()
-    print("Введите основание системы счисления этого числа:")
-    num_sys1 = input()
-    print("Введите основание желаемой системы счисления:")
-    num_sys2 = input()
-    num_sys_conv(number, num_sys1, num_sys2)
+    number = int(input("Введите число для конвертации:"))
+    num_sys1 = int(input("Введите основание системы счисления этого числа:"))
+    num_sys2 = int(input("Введите основание желаемой системы счисления:"))
+
+    if num_sys2 == 10:
+        bin2dec(number)
+    elif num_sys2 == 2:
+        dec2bin(number)
+    else:
+        print(f"Программа не умеет переводить числа в систему счисления с основанием {num_sys2}")
     print("Конец программы")
